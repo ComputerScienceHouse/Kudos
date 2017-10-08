a68bb9005a963a3f9cf77584cc0fc5c9d9cc4de8
Trevor Sherrard <sherrardtr@gmail.com>
2017-10-08 02:58:26 -0400

    Add arduino sketch with initial ROS support
    Dynamic memory constraints resulted in ROS
    functionality being split from XBOX/USB support.

---
+++ KudosROS/KudosROS.ino
@@ -0,0 +1,83 @@
+#include <Servo.h>
+
+#include "ros.h"
+#include "geometry_msgs/Twist.h"
+
+ros::NodeHandle nh;
+
+Servo leftMotor;
+Servo rightMotor;
+
+const int PIN_MOTOR_LEFT = 5;
+const int PIN_MOTOR_RIGHT = 6;
+
+float x_pos = 0;
+float theta = 0;
+
+void velCallback(const geometry_msgs::Twist& vel);
+
+ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel" , velCallback);
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println("Started setup()");
+    Serial.println("Attaching Motors");
+    leftMotor.attach(PIN_MOTOR_LEFT);
+    rightMotor.attach(PIN_MOTOR_RIGHT);
+    Serial.println("setting up ROS communications");
+    nh.initNode();
+    nh.subscribe(sub);
+    Serial.println("Finished setup()");
+}
+
+void loop() {
+    nh.spinOnce();
+    delay(10);
+}
+
+//Input on a scale of 0-180
+void arcadeDrive(int16_t y, int16_t x) {
+    Serial.print("Y power: ");
+    Serial.print(y);
+    Serial.print(", X power: ");
+    Serial.print(x);
+
+    //Set left and right to y power
+    int leftPower = y;
+    int rightPower = y;
+
+    //Balance x around 0 (range -90 to 90).
+    x -= 90;
+    leftPower -= x;
+    rightPower += x;
+
+    //Trim range for left
+    if (leftPower > 180) {
+        leftPower = 180;
+    } else if (leftPower < 0) {
+        leftPower = 0;
+    }
+
+    //Trim range for right
+    if (rightPower > 180) {
+        rightPower = 180;
+    } else if (rightPower < 0) {
+        rightPower = 0;
+    }
+    rightPower = 180 - rightPower;
+
+    Serial.print(", Left power: ");
+    Serial.print(leftPower);
+    Serial.print(", Right power: ");
+    Serial.print(rightPower);
+      
+    leftMotor.write(leftPower);
+    rightMotor.write(rightPower);
+    
+}
+
+void velCallback(const geometry_msgs::Twist& vel){
+    x_pos = vel.linear.x;
+    theta = vel.angular.z;
+}
+
